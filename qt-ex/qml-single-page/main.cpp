08ea2cb4235b752e55d1af1d70f3864e45434191
Emery Goss (AIVAS-v2:Ubuntu-22.04) <m.goss792@gmail.com>
2025-10-09 08:59:31 -0500

    initial logic

--- qt-ex/qml-single-page/main.cpp
+++ qt-ex/qml-single-page/main.cpp
@@ -1,2 +1,40 @@
 
-int main () {}
+#include <cstdlib>
+
+#include <QtCore/QCommandLineParser>
+#include <QtCore/QCoreApplication>
+#include <QtCore/QString>
+#include <QtCore/QtConfig>
+#include <QtCore/QUrl>
+#include <QtGui/QGuiApplication>
+#include <QtQml/QQmlApplicationEngine>
+
+int main (int argc, char* argv[]) {
+    using Qt::Literals::StringLiterals::operator""_s;
+
+    QCoreApplication::setApplicationName("Qt Quick PDF Viewer Example");
+    QCoreApplication::setOrganizationName("QtProject");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+    QGuiApplication app{argc, argv};
+
+    QCommandLineParser parser{};
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("file", "The file to open.");
+    parser.process(app);
+
+    QQmlApplicationEngine engine{};
+
+    QUrl to_load{"qrc:/singlepage/resources/test.pdf"};
+    if (not parser.positionalArguments().isEmpty()) {
+        to_load = QUrl::fromLocalFile(parser.positionalArguments().constFirst());
+    }
+    engine.setInitialProperties({{"source", to_load}});
+
+    engine.load(QUrl{u"qrc:/qt/qml/singlepage/viewer.qml"_s});
+    if (engine.rootObjects().isEmpty()) {
+        return EXIT_FAILURE;
+    }
+
+    return app.exec();
+}
--- qt-ex/qml-single-page/viewer.qml
+++ qt-ex/qml-single-page/viewer.qml
@@ -2,5 +2,11 @@ import QtQuick.Controls
 
 ApplicationWindow {
     id: root
+    width: 800
+    height: 1024
+    color: "lightgrey"
+    // title: document.title
+    visible: true
     required property url source  // for main.cpp
+    property real scaleStep: Math.sqrt(2)
 }
