7f412a2172f802963f66d0520c6ce491617805c9
mry792 <m.goss792@gmail.com>
2025-10-18 00:13:32 -0500

    feat(qml-multipage): add main body

--- qt-ex/qml-multipage/main.cpp
+++ qt-ex/qml-multipage/main.cpp
@@ -1,6 +1,37 @@
 
-#include <cstdlib>
+#include <QtCore/QCoreApplication>
+#include <QtCore/QDir>
+#include <QtCore/QUrl>
+#include <QtCore/QtVersion>
+#include <QtQml/QQmlApplicationEngine>
+#include <QtCore/QString>
 
-int main () {
-    return EXIT_SUCCESS;
+#include "PdfApplication.hpp"
+
+int main (int argc, char* argv[]) {
+    using Qt::Literals::StringLiterals::operator ""_s;
+
+    QCoreApplication::setApplicationName("Qt Quick Multi-page PDF Viewer Example");
+    QCoreApplication::setOrganizationName("QtProject");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+    qml_multipage::PdfApplication app{argc, argv};
+
+    QQmlApplicationEngine engine{};
+    engine.load(QUrl{u"qrc:/qt/qml/multipage/viewer.qml"_s});
+    auto* root = engine.rootObjects().constFirst();
+    app.file_opener(root);
+
+    if (app.arguments().count() > 1) {
+        // Alternatively, use QUrl::fromLocalFile() - network loading is not supported yet.
+        auto to_load = QUrl::fromUserInput(
+            app.arguments().at(1),
+            QDir::currentPath(),
+            QUrl::AssumeLocalFile
+        );
+        root->setProperty("source", to_load);
+    } else {
+        root->setProperty("source", u"qrc:/multipage/resources/test.pdf"_s);
+    }
+
+    return app.exec();
 }
--- qt-ex/qml-multipage/viewer.qml
+++ qt-ex/qml-multipage/viewer.qml
@@ -1,4 +1,10 @@
 import QtQuick.Controls
 
 ApplicationWindow {
+    id: root
+    width: 800
+    height: 1024
+    color: "lightgrey"
+    visible: true
+    property string source  // for main.cpp
 }
